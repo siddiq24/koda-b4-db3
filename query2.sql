0dda5222402fff734ba0cec13e36d5a1d24fab8a
siddiq24 <siddiqrm24@gmail.com>
2025-10-29 13:23:03 +0700

    fix: change joined directors_genres to movies_genres

--- query2.sql
+++ query2.sql
@@ -1,8 +1,8 @@
-select m.id, m.name as "movie_name", d.first_name as "director", dg.genre
+select m.id, m.name as "movie_name", d.first_name as "director", mg.genre
 from movies as m
 JOIN movies_directors as md ON md.movie_id = m.id
 JOIN directors as d on md.director_id = d.id
-JOIN directors_genres as dg on dg.director_id = d.id
+JOIN movies_genres as mg on mg.movie_id = m.id
 limit 50;
 
 SELECT a.first_name, m.name as "movies name", r.role
