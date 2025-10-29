973f772fda8b21907034834fa361e89e358dfe3f
siddiq24 <siddiqrm24@gmail.com>
2025-10-29 13:11:08 +0700

    fix: join table directors_genres to task number 1

--- query2.sql
+++ query2.sql
@@ -1,12 +1,11 @@
-select m.id, m.name, m.rankscore, d.first_name
+select m.id, m.name as "movie_name", d.first_name as "director", dg.genre
 from movies as m
 JOIN movies_directors as md ON md.movie_id = m.id
 JOIN directors as d on md.director_id = d.id
+JOIN directors_genres as dg on dg.director_id = d.id
 limit 50;
 
-SELECT a.first_name, m.name, r.role
+SELECT a.first_name, m.name as "movies name", r.role
 from actors as a
 left JOIN roles as r ON r.actor_id = a.id
-left JOIN movies as m ON r.movie_id = m.id
-GROUP BY a.first_name, m.name, r.role;
-
+left JOIN movies as m ON r.movie_id = m.id;
